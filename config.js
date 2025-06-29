c1268e5e3603e74e1d95730255cf4d9fe837f224
supuntha520 <supuntha50@gmail.com>
2025-06-29 18:27:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Manu-MD&fB90mAKS#6fN9pZ2Sv-p5BHFKVfCEgk0YyZ65E3DKTfdxc6jUAFY', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&eF1AnJyI#9-1QbIB4LIDAsL-gCX7-iolYFIL_NQUT2zMwNgrr3l0', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'mongodb+srv://kepika9203:paC1mC8sprtrNT3r@supuntha.tlmcffn.mongodb.net/' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
